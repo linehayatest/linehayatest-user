74fbe1704ebc9ff818fcb9732b1efbbd7a21abc1
Alex Araiza <alexandre.araiza@gmail.com>
2021-10-20 18:24:16 +0200

    Fix useSetupPeer return value

--- src/features/call/hooks/useSetupPeer.ts
+++ src/features/call/hooks/useSetupPeer.ts
@@ -26,48 +26,57 @@ function useSetupPeer() {
   const toast = useToast()
 
   return ({ peer, localAudio, remoteAudio }: setupPeerProps) => {
-    navigator.mediaDevices.getUserMedia({ video: false, audio: { echoCancellation: true }})
+    if (localStream === null) {
+      navigator.mediaDevices.getUserMedia({ video: false, audio: { echoCancellation: true }})
       .then(stream => {
         setStream(stream)
         localAudio.current.srcObject = stream
         localAudio.current.autoplay = false
-        peer.on('open', (id: string) => {
-          setPeerId(id)
-          requestCall()
-        })
-        peer.on('call', function(call) {
-          call.answer(stream)
-          call.peerConnection.oniceconnectionstatechange = function() {
-            if (call.peerConnection.iceConnectionState === 'disconnected') {
-              setUserState('idling')
-              toast({
-                description: "Volunteer has hanged up",
-                status: "error"
-              })
-              call.close()
-            }
-          }
-          call.on('stream', function(stream) {
-            setPeerConn(call)
-            localAudio.current.muted = true
-            remoteAudio.current.muted = false
-            remoteAudio.current.srcObject = stream 
-            remoteAudio.current.autoplay = true
-            setUserState('calling')
-          })
-        })
+        requestCall()
+      })
+      .catch(err => console.error("Unable to get user media" + err))
+    }
 
-        peer.on('connection', function(connection) {
-          connection.on('close', function() {
-            setUserState('idling')
-            history.push('/')
-            toast({
-              description: "Volunteer has hanged up",
-              status: "error"
-            })
+    peer.on('open', (id: string) => {
+      setPeerId(id)
+      if (localStream !== null) {
+        requestCall()
+      }
+    })
+
+    peer.on('call', call => {
+      call.answer(localStream)
+      call.peerConnection.oniceconnectionstatechange = () => {
+        if (call.peerConnection.iceConnectionState === 'disconnected') {
+          setUserState('idling')
+          toast({
+            description: "Volunteer has hanged up",
+            status: "error"
           })
+          call.close()
+        }
+      }
+
+      call.on('stream', remoteStream => {
+        setPeerConn(call)
+        localAudio.current.muted = true
+        remoteAudio.current.muted = false
+        remoteAudio.current.srcObject = remoteStream 
+        remoteAudio.current.autoplay = true
+        setUserState('calling')
+      })
+    })
+
+    peer.on('connection', connection => {
+      connection.on('close', () => {
+        setUserState('idling')
+        history.push('/')
+        toast({
+          description: "Volunteer has hanged up",
+          status: "error"
         })
-      }).catch(err => console.error("Uable to get user media" + err))
+      })
+    })
   }
 }
 
